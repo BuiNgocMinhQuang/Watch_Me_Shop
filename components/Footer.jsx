a8786675a246070d98eca3939828407998a247f6
Bui Ngoc Minh Quang <buingocminhquang@gmail.com>
2022-09-21 14:59:36 +0700

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiFillFacebook,
+  AiFillYoutube,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 My Catch All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiFillFacebook />
+        <AiFillYoutube />
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
